51196dc2dcf9b538e699844f060ed0b58367e228
Monica Larre <monica.larre@tec.mx>
2022-06-24 13:25:57 -0500

    agregue funcion print3Hola desde main

--- prueba.py
+++ prueba.py
@@ -7,10 +7,13 @@ def print2Hola():
     print("hola")
     print("El hola escrito desde el remoto")
 
+def print3Hola():
+    print("hola", "hola", "hola")
 
 def main():
     printHola()
     print2Hola()
+    print3Hola()
     pass
 
 main()
