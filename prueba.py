340f3420c8aa7f3f09705362b0bb565f8c8f131e
Monica Larre <monica.larre@tec.mx>
2022-06-24 11:54:07 -0500

    desde refact agrego un nuevo print a print2hola

---
+++ prueba.py
@@ -0,0 +1,16 @@
+# esta es una prueba
+
+def printHola():
+    print("hola")
+
+def print2Hola():
+    print("hola")
+    print("hola de nuevo")
+
+
+def main():
+    printHola()
+    print2Hola()
+    pass
+
+main()
